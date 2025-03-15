ffae89ab6e7cc64e2c27d12989e0baac37c38c1c
samiha-mahin <samihamahin36@gmail.com>
2025-03-15 23:11:42 +0600

    d

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,23 @@
-
-
-const App = () => {
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter} from "react-router-dom";
+import { themeSettings } from "./theme";
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    <div>App</div>
-  )
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            hiii
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
