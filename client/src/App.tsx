cf81e8ebb87e27386c582541256e574c6ed1aa26
samiha-mahin <samihamahin36@gmail.com>
2025-03-16 15:54:13 +0600

    d

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { BrowserRouter} from "react-router-dom";
 import { themeSettings } from "./theme";
+import Navbar from "@/scenes/navbar";
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
@@ -12,7 +13,7 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-            hiii
+           <Navbar/>
           </Box>
         </ThemeProvider>
       </BrowserRouter>
