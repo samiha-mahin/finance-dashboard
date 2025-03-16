b88ec13dbaf4ea22943a280837459c307cf96193
samiha-mahin <samihamahin36@gmail.com>
2025-03-16 16:03:47 +0600

    D

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import FlexBetween from '@/components/FlexBetween'
+import { Box, Typography, useTheme } from '@mui/material'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 type Props = {}
 
 const index = (props: Props) => {
+    const { palette } = useTheme();
   return (
-    <div>index</div>
+    <FlexBetween></FlexBetween>
   )
 }
 
