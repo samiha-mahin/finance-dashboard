1d8bb64e37951c4ed78b84068657c0e50770bf4d
samiha-mahin <samihamahin36@gmail.com>
2025-03-16 22:19:10 +0600

    dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,10 @@ import { Box } from "@mui/material";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import { themeSettings } from "./theme";
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard"
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
@@ -14,6 +15,9 @@ function App() {
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
            <Navbar/>
+           <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+           </Routes>
           </Box>
         </ThemeProvider>
       </BrowserRouter>
