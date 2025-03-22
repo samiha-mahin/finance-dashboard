e394004c9c243f7c43e7494d64b457b9db52f92f
samiha-mahin <samihamahin36@gmail.com>
2025-03-22 17:51:22 +0600

    dashboard

--- server/controllers/kpi_controller.js
+++ server/controllers/kpi_controller.js
@@ -1,12 +1,16 @@
 import { KPI } from "../models/kpi_model.js";
 
-export const kpiController = async (req,res)=>{
+export const kpiController = async (req, res) => {
     try {
+        res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+        res.setHeader("Expires", "0");
+        res.setHeader("Pragma", "no-cache");
+
         const kpis = await KPI.find();
         res.status(200).json(kpis);
     } catch (error) {
         return res.status(500).json({
-            message:  error.message,
+            message: error.message,
         });
     }
-}
+};
