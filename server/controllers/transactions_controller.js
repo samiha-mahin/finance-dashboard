07752a1367a6abb9cfbed3789ace6f9fa40461e9
samiha-mahin <samihamahin36@gmail.com>
2025-03-24 05:03:02 +0600

    dashboard

---
+++ server/controllers/transactions_controller.js
@@ -0,0 +1,17 @@
+import { transactions } from "../data/data.js"; // Import the static data from data.js
+
+export const transactionController = async (req, res) => {
+    try {
+        // Set headers to avoid caching
+        res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+        res.setHeader("Expires", "0");
+        res.setHeader("Pragma", "no-cache");
+
+        // Return the static data from data.js
+        res.status(200).json(transactions); // Return the static data as JSON response
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message, // Return error if something goes wrong
+        });
+    }
+};
